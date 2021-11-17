68205572993fdc952e09223ddbba7427aebef170
chillywr <alescey09@gmail.com>
2021-11-17 18:57:06 +0200

    fix if statement, add comments for debugging and renamed flag

--- iternal/argsParser.go
+++ iternal/argsParser.go
@@ -22,21 +22,25 @@ var (
 	}
 	flagsWithArg = map[string]interface{}{
 		"--filename": changeDefaultFilename,
-		"--timer":    changeDefaultTime,
+		"--time":     changeDefaultTime,
 	}
 )
 
 func ArgsHandler() {
 	args := os.Args
+
+	// For debug
+	// fmt.Println(args)
+	// args = []string{"./main", "--time", "10", "--filename", "extra.json", "-shuffle"}
+
 	for i := 1; i < len(args); i++ {
 		if value, ok := flags[args[i]]; ok {
 			value.(func())()
-		}
-		if valueWA, okWA := flagsWithArg[args[i]]; okWA && i+1 < len(args) {
+		} else if valueWA, okWA := flagsWithArg[args[i]]; okWA && i+1 < len(args) {
 			valueWA.(func(string))(args[i+1])
 			i++
 		} else {
-			fmt.Println("Invalid flag format\n" + flagsMassage)
+			fmt.Println("Invalid flag format\n\n" + flagsMassage)
 			os.Exit(1)
 		}
 
@@ -62,4 +66,5 @@ func shuffleQuizQAs() {
 
 func printHelpMessage() {
 	fmt.Println(helpMessage + "\n" + flagsMassage)
+	os.Exit(0)
 }
